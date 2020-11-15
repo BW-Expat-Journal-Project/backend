177af883f66d91bc31347d0801ebe95795e2af12
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-14 16:34:14 -0800

    update env

---
+++ api/config.js
@@ -0,0 +1,11 @@
+const knex = require("knex");
+
+const knexfile = require("../knexfile");
+
+//on the Heroku NODE_ENV will be 'production
+
+const enviroment = process.env.NODE_ENV || "development";
+
+const config = knexfile(enviroment);
+
+module.exports = knex(config);
