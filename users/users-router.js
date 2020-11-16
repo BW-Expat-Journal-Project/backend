88de56cba5b53903de2015ded7d342faffbeda23
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-16 14:24:37 -0800

    update |GET users/:id/posts|

---
+++ users/users-router.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+const Users = require("./users-model");
+const restricted = require("../auth/auth-middleware");
+
+
+router.get("/", restricted,  (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+//GET user's posts
+router.get('/:id/posts', (req, res) => {
+  Users.findById(req.params.id)
+  .then(user =>{
+      if(user){
+          Posts.findBy({user_id: user.id}).then(posts =>{
+              res.status(200).json({notification:`Information of ${user.username} post`, information: posts})
+          })
+          .catch(err =>{
+              res.status(401).json({message: `Failed To Find Posts For: ${user.username}`, errMessage: err.message})
+          })
+      } else{
+          res.status(401).json({message: `Failed To Find User With ID: ${req.params.id}`})
+      }
+  })
+  .catch(err => {
+      res.status(500).json({message: 'Failed To Get Users Posts', errMessage:err.message})
+  })
+})
+
+module.exports = router;
