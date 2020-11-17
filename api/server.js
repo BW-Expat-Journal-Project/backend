c153311b7e710d3839e340f4696cd2ddeffb8c8c
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-16 17:01:54 -0800

    update api posts server

--- api/server.js
+++ api/server.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet'); 
-const morgan = require ("morgan");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 //router
-const authRouter = require('../auth/auth-router')
-const postRouter = require('../posts/posts-router')
+const authRouter = require("../auth/auth-router");
+const postRouter = require("../posts/posts-router");
 
 //mwauthorization
-const restrictedmw = require('../auth/auth-middleware')
+const restrictedmw = require("../auth/auth-middleware");
 
 //server
 const server = express();
 
 //router
 server.use(cors());
-server.use(express.json()); 
-server.use('/',helmet());
-server.use('/', morgan('--API testing for Expat Journal BuildWeek--'))
+server.use(express.json());
+server.use("/", helmet());
+server.use("/", morgan("--API testing for Expat Journal BuildWeek--"));
 
 //routes
-server.use('/api/auth', authRouter)
-server.use('api/posts',restrictedmw, postRouter )
-server.get('/', (req, res) =>{
-    res.status(200).json({server: 'Expat Journal BuildWeek'})
-})
+server.use("/api/auth", authRouter);
+server.use("/api/posts", restrictedmw, postRouter);
+server.get("/", (req, res) => {
+  res.status(200).json({ server: "Expat Journal BuildWeek" });
+});
 
 module.exports = server;
