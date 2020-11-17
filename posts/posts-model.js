00235d1b5e674fec8376daa08b354d0e956fa9b0
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-16 16:49:16 -0800

    update posts endpoints

--- posts/posts-model.js
+++ posts/posts-model.js
@@ -1,33 +1,50 @@
-const db = require('../api/config')
+const db = require("../api/config");
 
 module.exports = {
-    find, 
-    findBy,
-    findById, 
-    insert, 
-    update,
-    remove
-}
-function find(){
-
-}
-
-function findBy(){
-
-}
-
-function findById(){
-
-}
-
-function insert(){
-
+  find,
+  findBy,
+  findById,
+  insert,
+  update,
+  remove,
+};
+//Find all posts data
+function find() {
+  return db("posts").select(
+    "id",
+    "user_id",
+    "title",
+    "description",
+    "photo_url"
+  );
 }
 
-function update(){
-
+//Find posts by id
+function findById(id) {
+    return db("posts")
+    .select("id", "user_id", "title", "description", "photo_url")
+    .where({ id })
+    .first();
 }
 
-function remove(){
+//insert new post into db
+function insert(newPost) {
+    return db("posts")
+    .insert(newPost).returning(["title", "description", "photo_url"]);
+}
 
+
+//Find posts by filter
+function findBy(filter) {
+  return db("posts").where(filter);
+}
+
+//update new infor of pj to db
+function update(changes, id) {
+  return db("posts").where({ id }).update(changes); //updates the record with 'changes' where the id matches
+}
+
+//remove post from database
+function remove(id) {
+  return db("posts").where({ id }).del();
 }
