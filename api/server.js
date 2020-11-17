d24e2254fa6b1868209335c83435fa9f106da214
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-16 16:58:01 -0800

    update posts server

--- api/server.js
+++ api/server.js
@@ -3,16 +3,25 @@ const cors = require('cors');
 const helmet = require('helmet'); 
 const morgan = require ("morgan");
 
+//router
 const authRouter = require('../auth/auth-router')
+const postRouter = require('../posts/posts-router')
 
+//mwauthorization
+const restrictedmw = require('../auth/auth-middleware')
+
+//server
 const server = express();
 
+//router
 server.use(cors());
 server.use(express.json()); 
 server.use('/',helmet());
 server.use('/', morgan('--API testing for Expat Journal BuildWeek--'))
-server.use('/api/auth', authRouter)
 
+//routes
+server.use('/api/auth', authRouter)
+server.use('api/posts',restrictedmw, postRouter )
 server.get('/', (req, res) =>{
     res.status(200).json({server: 'Expat Journal BuildWeek'})
 })
